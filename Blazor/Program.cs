7b600e0b15937012a413854f0a8b5a1fe16bc65a
Jesper <jw_jw@hotmail.com>
2025-03-10 09:31:34 +0100

    init

---
+++ Blazor/Program.cs
@@ -0,0 +1,36 @@
+using Blazor.Components;
+
+namespace Blazor;
+
+class Program
+{
+    static void Main()
+    {        
+        var builder = WebApplication.CreateBuilder();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
