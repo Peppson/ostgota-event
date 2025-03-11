0a359c40aea8375f6d37532addfbcfe42c1edd44
Jesper <jw_jw@hotmail.com>
2025-03-11 15:46:20 +0100

    Db structured

---
+++ Core/Data/Database.cs
@@ -0,0 +1,8 @@
+﻿namespace Data;
+
+public class Database : IDatabase
+{
+    public List<Event> Events { get; set; } = [];
+    public List<User> Users { get; set; } = [];
+    public List<Ticket> Tickets { get; set; } = [];
+}
