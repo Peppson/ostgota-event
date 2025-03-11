dc684e5e960ce26adf8e5a954ffbc8c030eb4483
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-03-11 15:43:27 +0100

    Reworked the auth-endpoints to controllers. Separated the response and requestmodels to reduce codeduplication. Added nugetpackage of Bcrypt to Core-project

---
+++ Api/Controllers/Auth/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Core.Services;
+using Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Auth
+{
+    public class RegisterController : Controller
+    {
+
+        public static Results<Ok<Response>, BadRequest<string>> Handle( Request request, IAuthService authService)
+        {
+            var result = authService.Register(request.Username, request.Password);
+            if (result == null)
+            {
+                return TypedResults.BadRequest("Username already exists");
+            }
+            var response = new Response(result.Username, result.Role);
+            return TypedResults.Ok(response);
+        }
+    }
+}
+
