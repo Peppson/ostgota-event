7a186e05af80f13531afc3b0d3927497e43e810d
Jesper <jw_jw@hotmail.com>
2025-03-12 07:12:13 +0100

    add more links

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -11,7 +11,13 @@ public class User
     [Required]
     public required string Email { get; set; }
     public string? PhoneNumber { get; set; }
-    public UserRoles Role { get; set; } = UserRoles.User;
+    public UserRole Role { get; set; } = UserRole.User;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public List<Ticket> Tickets { get; set; } = [];
+}
+
+public enum UserRole
+{
+    Admin,
+    User
 }
