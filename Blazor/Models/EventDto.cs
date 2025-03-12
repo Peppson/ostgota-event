ced9bb3eed40af336b11828e69620147e89d2ad3
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-03-12 11:45:59 +0100

    Added admin.razor page. Added divs for holding usersearch, ticketsearch and eventsearch plus statistic of numbers. Added DTO-models for tickets, events and users in order to only get relevantdata.

---
+++ Blazor/Models/EventDto.cs
@@ -0,0 +1,28 @@
+﻿using Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Models
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string Description { get; set; }
+        [Required]
+        public required string City { get; set; }
+        [Required]
+        public AccessType AccessType { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public int TicketsSold { get; set; }
+        [Required]
+        public int TicketsMax { get; set; }
+        public bool HasSeat { get; set; } = false;
+        public string? ImagePath { get; set; }  // What to do if no path? Default background?
+    }
+}
