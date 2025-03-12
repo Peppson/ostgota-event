3b04777e4006b044d7e44ffd87411da807ddd617
JockeBjers <joakim.bjerselius@hotmail.com>
2025-03-12 10:04:28 +0100

    Make tickets private in User, access through methods

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -1,7 +1,7 @@
-namespace Data.Models;
+namespace Core.Models;
 
 public class User
-{   
+{
     [Key]
     public int Id { get; set; }
     [Required]
@@ -13,7 +13,38 @@ public class User
     public string? PhoneNumber { get; set; }
     public UserRole Role { get; set; } = UserRole.User;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public List<Ticket> Tickets { get; set; } = [];
+
+    private List<Ticket> _tickets = new List<Ticket>();
+    public IReadOnlyCollection<Ticket> Tickets => _tickets.AsReadOnly();
+
+    public void BuyTickets(Event currentEvent, int quantity, decimal ticketPrice, string? seat = null)
+    {
+        if (quantity <= 0)
+            throw new ArgumentException("Köp måste vara mer än ett");
+
+        if (currentEvent.RemainingTickets < quantity)
+            throw new InvalidOperationException("Inte tillräckligt många biljetter kvar!");
+
+
+        // Register new tickets
+        for (int i = 0; i < quantity; i++)
+        {
+            var ticket = new Ticket 
+            {
+                UserId = this.Id,
+                User = this,
+                EventId = currentEvent.Id,
+                Event = currentEvent,
+                Price = ticketPrice,
+                Seat = seat
+            };
+
+            _tickets.Add(ticket);
+        }
+
+        currentEvent.RegisterTicket(quantity);
+    }
+
 }
 
 public enum UserRole
