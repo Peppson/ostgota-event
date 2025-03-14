fe849ece8dc6e97a4c25015df68cff75b3c783f5
JockeBjers <joakim.bjerselius@hotmail.com>
2025-03-14 10:59:56 +0100

    Apply related logic to the eventValidator

--- Api/Controllers/Events/EventValidator.cs
+++ Api/Controllers/Events/EventValidator.cs
@@ -10,8 +10,14 @@ public class EventValidator : AbstractValidator<Event>
         RuleFor(x => x.Adress).NotEmpty();
         RuleFor(x => x.StartTime).NotEmpty();
         RuleFor(x => x.EndTime).GreaterThan(x => x.StartTime);
-        RuleFor(x => x.TicketsMax).GreaterThan(0);
-        RuleFor(x => x.TicketsSold).LessThanOrEqualTo(x => x.TicketsMax);
+
+        RuleFor(x => x.TicketsMax)
+          .GreaterThan(0)
+          .When(x => x.AccessType != AccessType.Free || x.HasSeat);
+
+        RuleFor(x => x.TicketsSold)
+            .LessThanOrEqualTo(x => x.TicketsMax ?? int.MaxValue);
+
         RuleFor(x => x.RemainingTickets).GreaterThanOrEqualTo(0);
         RuleFor(x => x.IsSoldOut).Equal(x => x.RemainingTickets == 0);
         RuleFor(x => x.AccessType).IsInEnum();
