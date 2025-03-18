38c0518bb47f6c7011d2387d53229aab1fb2f73f
Viktor <viktor.e.thorn@hotmail.com>
2025-03-18 13:32:27 +0100

    Added validation into models in blazorstandaloneproject. Added DataAnnotationValidation in forms in order to correctly validate

--- BlazorStandAlone/Models/EventDto.cs
+++ BlazorStandAlone/Models/EventDto.cs
@@ -5,24 +5,42 @@ namespace BlazorStandAlone.Models;
 public class EventDto
 {
     public int Id { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(30, ErrorMessage = "Name cannot be longer than 30 characters")]
     public required string Name { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "Description is required")]
+    [StringLength(100, ErrorMessage = "Description cannot be longer than 100 characters")]
     public required string Description { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "City is required")]
+    [StringLength(40, ErrorMessage = "City cannot be longer than 40 characters")]
     public required string City { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "Address is required")]
+    [StringLength(40, ErrorMessage = "Address cannot be longer than 40 characters")]
     public required string Address { get; set; }
+    
+    [Required(ErrorMessage = "Access type is required"), EnumDataType(typeof(AccessType), ErrorMessage = "Invalid access type")]
     public AccessType AccessType { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "Start time is required")]
     public DateTime StartTime { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "End time is required")]
     public DateTime EndTime { get; set; }
-    [Required]
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Maximum tickets must be a positive number")]
     public int? TicketsMax { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Tickets sold must be a positive number")]
     public int TicketsSold { get; set; }
-    [Required]
+    
+    [Required(ErrorMessage = "HasSeat value is required")]
     public bool HasSeat { get; set; }
+    
+    [Required(ErrorMessage = "Image path is required"), Url(ErrorMessage = "Invalid URL")]
     public required string ImagePath { get; set; } 
 }
 
