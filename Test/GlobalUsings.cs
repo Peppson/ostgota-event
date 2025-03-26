83ab485ccafdf1f3d5e4a01dd9fd7bba95fded7f
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-03-26 15:09:29 +0100

    cleaned up test-folder

--- Test/GlobalUsings.cs
+++ Test/GlobalUsings.cs
@@ -7,8 +7,5 @@ global using Core.Services.Users;
 global using Core.Models;
 global using Reqnroll;
 global using Core.Data;
-global using Core.Services.Tickets;
-global using Core.Services.Events;
-global using Core.Services.Auth;
 
 
